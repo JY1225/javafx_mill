fc127c173a914065b31ba4c3970b1003acec67c7
Administrator <Administrator@PC-201810291710>
2018-12-07 09:32:07 +0800

    merger

---
+++ mill/src/cn/greatoo/easymill/ui/configure/robot/RobotGriperViewController.java
@@ -0,0 +1,41 @@
+package cn.greatoo.easymill.ui.configure.robot;
+
+import javafx.fxml.FXML;
+
+import javafx.scene.control.Button;
+
+import javafx.scene.layout.HBox;
+
+import javafx.event.ActionEvent;
+
+import javafx.scene.control.ScrollPane;
+
+import javafx.scene.layout.GridPane;
+
+public class RobotGriperViewController {
+	@FXML
+	private GridPane gridPane;
+	@FXML
+	private Button editBt;
+	@FXML
+	private Button newBt;
+	@FXML
+	private ScrollPane scrollPane;
+	@FXML
+	private HBox hbox;
+
+	public void init() {
+		
+		
+	}
+	@FXML
+	public void editBtAction(ActionEvent event) {
+		
+	}
+	
+	@FXML
+	public void newBtAction(ActionEvent event) {
+		
+	}
+	
+}
