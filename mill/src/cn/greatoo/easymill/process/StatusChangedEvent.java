745f056c0cfeaeeb55532b0baaa3db9343980669
Administrator <Administrator@PC-201810291710>
2018-12-29 12:04:04 +0800

    22222

--- mill/src/cn/greatoo/easymill/process/StatusChangedEvent.java
+++ mill/src/cn/greatoo/easymill/process/StatusChangedEvent.java
@@ -17,7 +17,6 @@ public class StatusChangedEvent {
 	public static final int TEACHING_NEEDED = 8;
 	public static final int TEACHING_FINISHED = 9;
 	public static final int FINISHED = 10;	
-
 	private int statusId;
 	
 	public StatusChangedEvent(final int statusId) {
