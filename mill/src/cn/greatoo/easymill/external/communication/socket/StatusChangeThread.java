3ed67788d6c85a8673940e22cf359e3414051b42
JY <JY@PC-201810291710>
2019-01-24 14:31:34 +0800

    conn

--- mill/src/cn/greatoo/easymill/external/communication/socket/StatusChangeThread.java
+++ mill/src/cn/greatoo/easymill/external/communication/socket/StatusChangeThread.java
@@ -20,7 +20,7 @@ public class StatusChangeThread implements Runnable {
 
 	private FanucRobot robot;
 	private CNCMachine cncMachine;
-	private double previousXRest,previousYRest,previousZRest;
+	private double previousXRest, previousYRest, previousZRest;
 	private int rpreviousStatus;
 	private boolean alive;
 	private AlarmListenThread alarmListenThread;
@@ -29,22 +29,21 @@ public class StatusChangeThread implements Runnable {
 	private Set<Integer> previousActiveMCodes;
 
 	public StatusChangeThread(final Button AlarmButton, final int duration, ButtonStyleChangingThread changingThread) {
-		this.alarmListenThread = new AlarmListenThread(AlarmButton, duration, changingThread);	
-		alarmListenThread.run();
+		this.alarmListenThread = new AlarmListenThread(AlarmButton, duration, changingThread);
 		this.previousStatus = new HashMap<Integer, Integer>();
 		this.previousActiveMCodes = new HashSet<Integer>();
-		this.alive = true;		
+		this.alive = true;
 		connCNC();
 		connRobo();
-		conn();
 	}
 
 	@Override
 	public void run() {
 		while (alive) {
-			try {	
+			try {
 				alarmListenThread.run();
-				if (robot != null) {
+				conn();
+				if (robot != null && robot.isConnected()) {
 					robot.updateStatusRestAndAlarms();
 					int statu = robot.getStatus();
 					if (statu != rpreviousStatus) {
@@ -57,18 +56,19 @@ public class StatusChangeThread implements Runnable {
 					if (zrest != previousZRest || xrest != previousXRest || yrest != previousYRest) {
 						if (xrest > 1 || yrest > 1 || zrest > 1) {
 							Platform.runLater(new Runnable() {
-								@Override public void run() {
-									String str = " (移动 (" + xrest + ", " + yrest + ", "+ zrest + ")"+ ")";
-									TeachAndAutoThread.getView().setMessege(Controller.getMessege()+str);							
-									//System.out.println("xrest: "+xrest+"; yrest: "+yrest+"; zrest: "+zrest);
-								} 
+								@Override
+								public void run() {
+									String str = " (移动 (" + xrest + ", " + yrest + ", " + zrest + ")" + ")";
+									TeachAndAutoThread.getView().setMessege(Controller.getMessege() + str);
+									// System.out.println("xrest: "+xrest+"; yrest: "+yrest+"; zrest: "+zrest);
+								}
 							});
-						}					
-					} 					
+						}
+					}
 				}
 
-				if (cncMachine != null) {
-					//更新状态和MCode
+				if (cncMachine != null && cncMachine.isConnected()) {
+					// 更新状态和MCode
 					cncMachine.updateStatusAndAlarms();
 					boolean statusChanged = false;
 					// cncMachine.statusChanged();
@@ -79,7 +79,7 @@ public class StatusChangeThread implements Runnable {
 					}
 					Set<Integer> activeMCodes = new HashSet<Integer>();
 					activeMCodes = cncMachine.getMCodeAdapter().getActiveMCodes();
-					//如果状态变或者MCode变通知线程继续进行
+					// 如果状态变或者MCode变通知线程继续进行
 					if ((statusChanged) || (!previousActiveMCodes.containsAll(activeMCodes))
 							|| (!activeMCodes.containsAll(previousActiveMCodes))) {
 
@@ -91,67 +91,56 @@ public class StatusChangeThread implements Runnable {
 
 				Thread.sleep(250);
 
-			} catch (InterruptedException | SocketResponseTimedOutException | SocketDisconnectedException | SocketWrongResponseException e) {
+			} catch (InterruptedException | SocketResponseTimedOutException | SocketDisconnectedException
+					| SocketWrongResponseException e) {
 				interrupted();
-			} 
+			}
 		}
 	}
 
 	private void conn() {
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (isRunning) {
-					if (alarmListenThread != null && robot != null && cncMachine != null) {
-						boolean isRobotConn = robot.isConnected();
-						alarmListenThread.setIsRobotConn(isRobotConn);
-						if (!isRobotConn) {
-							connRobo();
-						}
-						boolean isCNCConn = cncMachine.isConnected();
-						alarmListenThread.setIsCNCConn(isCNCConn);
-						if (!isCNCConn) {
-							isCNCConn = connCNC();
-						}
-						if (isCNCConn && isRobotConn) {
-							alarmListenThread.setIsRobotConn(isRobotConn);
-							alarmListenThread.setIsCNCConn(isCNCConn);
-							isRunning = false;
-						}
-					}
-					try {
-						Thread.sleep(500);
-					} catch (Exception e) {
-
-					}
-				}
+		if (alarmListenThread != null && robot != null && cncMachine != null) {
+			boolean isRobotConn = robot.isConnected();
+			alarmListenThread.setIsRobotConn(isRobotConn);
+			if (!isRobotConn) {
+				connRobo();
 			}
-		}).start();
+			boolean isCNCConn = cncMachine.isConnected();
+			alarmListenThread.setIsCNCConn(isCNCConn);
+			if (!isCNCConn) {
+				connCNC();
+			}
+			if (isCNCConn && isRobotConn) {
+				alarmListenThread.setIsRobotConn(isRobotConn);
+				alarmListenThread.setIsCNCConn(isCNCConn);
+				isRunning = false;
+			}
+		}
 	}
 
 	protected boolean connCNC() {
 		try {
 			cncMachine = (CNCMachine) CNCHandler.getCNCMillingMachine();
-			if(cncMachine != null) {
+			if (cncMachine != null && cncMachine.isConnected()) {
 				cncMachine.indicateOperatorRequested(false);
 				cncMachine.indicateOperatorRequested(false);
 			}
-			return true;
-		} catch (SocketResponseTimedOutException | SocketDisconnectedException
-				| SocketWrongResponseException | InterruptedException e) {
+			return cncMachine.isConnected();
+		} catch (SocketResponseTimedOutException | SocketDisconnectedException | SocketWrongResponseException
+				| InterruptedException e) {
 			return false;
 		}
 	}
 
 	protected boolean connRobo() {
 		try {
-			robot = (FanucRobot)RobotHandler.getRobot();
-			if(robot != null) {
+			robot = (FanucRobot) RobotHandler.getRobot();
+			if (robot != null && robot.isConnected()) {
 				robot.restartProgram();
 			}
-			return true;
-		} catch (SocketDisconnectedException | SocketResponseTimedOutException
-				| SocketWrongResponseException | InterruptedException e) {
+			return robot.isConnected();
+		} catch (SocketDisconnectedException | SocketResponseTimedOutException | SocketWrongResponseException
+				| InterruptedException e) {
 			return false;
 		}
 	}
